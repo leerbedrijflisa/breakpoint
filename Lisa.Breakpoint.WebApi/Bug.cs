b5a1b5595be6207ca3dbdbe8fd8a39ee93ff8c65
baseenhoorn <baseenhoorn@hotmail.com>
2015-09-15 09:33:33 +0200

    Sql connection

---
+++ Lisa.Breakpoint.WebApi/Bug.cs
@@ -0,0 +1,10 @@
+﻿namespace Lisa.Breakpoint.WebApi
+{
+    public class Bug
+    {
+        public int      Id { get; set; }
+        public string   Title { get; set; }
+        public string   Description { get; set; }
+        public string   Status { get; set; }
+    }
+}
