9777a1410892b868278dec9ae45a473fab3cc80b
baseenhoorn <baseenhoorn@hotmail.com>
2015-09-15 14:29:11 +0200

    Sql connection is made with dummy data

---
+++ Lisa.Breakpoint.WebApi/BugController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Lisa.Breakpoint.WebApi
+{
+    public class BugController
+    {
+        static SqlDatabase _db = new SqlDatabase();
+
+        [HttpGet]
+        [Route("bugs")]
+        public object Get()
+        {
+            try
+            {
+                IList<Bug> bugs = _db.GetAllBugs();
+                return bugs;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        [HttpGet]
+        [Route("bugs/{id}")]
+        public object getBug(string id)
+        {
+            try
+            {
+                var bug = _db.GetBugById(id);
+
+                return bug;
+            }
+            catch (Exception)
+            {
+                return "error";
+            }
+        }
+
+        [HttpGet]
+        [Route("bugs/insert")]
+        public void insert(string id)
+        {
+            try
+            {
+                Bug bug = new Bug
+                {
+                    Title = "inserted bug",
+                    Description = "This bug is inserted with a query",
+                    Status = "not fixed"
+                };
+
+                _db.insertBug(bug);
+            }
+            catch (Exception)
+            {
+                // return "error";
+            }
+        }
+
+        [HttpGet]
+        [Route("bugs/update/{id}")]
+        public void update(string id)
+        {
+            try
+            {
+                Bug newBug = new Bug
+                {
+                    Title = "updated bug",
+                    Description = "This bug is updated with a query",
+                    Status = "fixed"
+                };
+
+                _db.updateBug(id, newBug);
+            }
+            catch (Exception)
+            {
+                // return "error";
+            }
+        }
+
+        [HttpGet]
+        [Route("bugs/delete/{id}")]
+        public void delete(string id)
+        {
+            try
+            {
+                _db.deleteBug(id);
+            }
+            catch (Exception)
+            {
+                // return "error";
+            }
+        }
+    }
+}
---
+++ Lisa.Breakpoint.WebApi/SqlDatabase.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Lisa.Breakpoint.WebApi
+{
+    public class SqlDatabase
+    {
+        private SqlConnection _conn;
+
+        private const string ConnString = "Data Source=(localdb)\\ProjectsV12;Initial Catalog=BreakPoint;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        private readonly string _selectSql = "SELECT * FROM dbo.bugs;";
+
+
+        public IList<Bug> GetAllBugs()
+        {
+            var bugs = new List<Bug>();
+            try
+            {
+                _conn = new SqlConnection(ConnString);
+                _conn.Open();
+
+                var cmd = new SqlCommand(_selectSql, _conn);
+                var rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    var bug = new Bug()
+                    {
+                        Id = rdr.IsDBNull(rdr.GetOrdinal("Id")) ? -1 : rdr.GetInt32(rdr.GetOrdinal("Id")),
+                        Title = rdr.IsDBNull(rdr.GetOrdinal("Title")) ? null : rdr.GetString(rdr.GetOrdinal("Title")),
+                        Description = rdr.IsDBNull(rdr.GetOrdinal("Description")) ? null : rdr.GetString(rdr.GetOrdinal("Description")),
+                        Status = rdr.IsDBNull(rdr.GetOrdinal("Status")) ? null : rdr.GetString(rdr.GetOrdinal("Status"))
+                    };
+                    bugs.Add(bug);
+                }
+            }
+            finally
+            {
+                _conn?.Close();
+            }
+            return bugs;
+        }
+
+        public Bug GetBugById(string id)
+        {
+            Bug bug = null;
+            try
+            {
+                _conn = new SqlConnection(ConnString);
+                _conn.Open();
+
+                const string sql = "SELECT * FROM dbo.bugs WHERE [Id] = @Id;";
+
+                var cmd = new SqlCommand(sql, _conn);
+
+                var paramId = new SqlParameter
+                {
+                    ParameterName = "@Id",
+                    Value = id
+                };
+                cmd.Parameters.Add(paramId);
+
+                var rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    bug = new Bug
+                    {
+                        Id = rdr.IsDBNull(rdr.GetOrdinal("Id")) ? -1 : rdr.GetInt32(rdr.GetOrdinal("Id")),
+                        Title = rdr.IsDBNull(rdr.GetOrdinal("Title")) ? null : rdr.GetString(rdr.GetOrdinal("Title")),
+                        Description = rdr.IsDBNull(rdr.GetOrdinal("Description")) ? null : rdr.GetString(rdr.GetOrdinal("Description")),
+                        Status = rdr.IsDBNull(rdr.GetOrdinal("Status")) ? null : rdr.GetString(rdr.GetOrdinal("Status"))
+                    };
+                }
+            }
+            finally
+            {
+                _conn?.Close();
+            }
+            return bug;
+        }
+
+        public long insertBug(Bug bug)
+        {
+            try
+            {
+                _conn = new SqlConnection(ConnString);
+
+                var cmd = _conn.CreateCommand();
+
+                cmd.CommandText =
+                    @"INSERT INTO[dbo].[Bugs] (Title, Description, Status) 
+                    VALUES(@Title, @Description,  @Status);SELECT CAST(scope_identity() AS int)";
+
+                cmd.Parameters.Add("@Title", SqlDbType.VarChar);
+                cmd.Parameters["@Title"].Value = bug.Title;
+
+                cmd.Parameters.Add("@Description", SqlDbType.VarChar);
+                cmd.Parameters["@Description"].Value = bug.Description;
+
+                cmd.Parameters.Add("@Status", SqlDbType.VarChar);
+                cmd.Parameters["@Status"].Value = bug.Status;
+
+                _conn.Open();
+
+                return long.Parse(cmd.ExecuteScalar().ToString());
+            }
+            finally
+            {
+                _conn?.Close();
+            }
+        }
+
+
+        public void updateBug(string id, Bug bug)
+        {
+            var bugToUpdate = GetBugById(id);
+            if (bugToUpdate == null)
+            {
+                throw new Exception("Bug does not exist in database");
+            }
+
+            try
+            {
+                _conn = new SqlConnection(ConnString);
+
+                var cmd = _conn.CreateCommand();
+                cmd.CommandText = @"UPDATE Bugs SET [Title]=@paramTitle, 
+                                                    [Description]=@paramDescription, 
+                                                    [Status]=@paramStatus
+                                                    WHERE Id=@Id";
+
+                cmd.Parameters.Add("@Id", SqlDbType.Int);
+                cmd.Parameters["@Id"].Value = id;
+
+                cmd.Parameters.Add("@paramTitle", SqlDbType.VarChar);
+                cmd.Parameters["@paramTitle"].Value = bug.Title;
+
+                cmd.Parameters.Add("@paramDescription", SqlDbType.VarChar);
+                cmd.Parameters["@paramDescription"].Value = bug.Description;
+
+                cmd.Parameters.Add("@paramStatus", SqlDbType.VarChar);
+                cmd.Parameters["@paramStatus"].Value = bug.Status;
+
+                _conn.Open();
+
+                var number = cmd.ExecuteNonQuery();
+
+                if (number != 1)
+                {
+                    throw new Exception($"No Bugs were updated with Id: {id}");
+                }
+            }
+            finally
+            {
+                _conn?.Close();
+            }
+        }
+
+
+        public void deleteBug(string id)
+        {
+            _conn = new SqlConnection(ConnString);
+
+            var sqlComm = _conn.CreateCommand();
+            sqlComm.CommandText = @"DELETE FROM bugs WHERE [ID] = @Id;";
+            sqlComm.Parameters.Add("@Id", SqlDbType.Int);
+            sqlComm.Parameters["@Id"].Value = id;
+
+            _conn.Open();
+
+            var rowsAffected = sqlComm.ExecuteNonQuery();
+
+            _conn.Close();
+
+            if (rowsAffected < 1)
+            {
+                throw new Exception("Entity has not been deleted!");
+            }
+        }
+    }
+}
