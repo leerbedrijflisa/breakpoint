6950b14f098a75129ff78ed63b502da1321625a9
baseenhoorn <baseenhoorn@hotmail.com>
2015-09-16 14:11:22 +0200

    Added: RavenDB connection. Removed: SQL conection.
    NOTE:
    you must install RavenDB (http://ravendb.net/downloads), and the packages needed in NuGet (RavenDB.Client & RavenDB.Server)

--- Lisa.Breakpoint.WebApi/BugController.cs
+++ Lisa.Breakpoint.WebApi/BugController.cs
@@ -7,92 +7,40 @@ namespace Lisa.Breakpoint.WebApi
     [Route("bugs")]
     public class BugController
     {
-        static SqlDatabase _db = new SqlDatabase();
+        static RavenDB _db = new RavenDB();
 
         [HttpGet]
         public object Get()
         {
-            try
-            {
-                IList<Bug> bugs = _db.GetAllBugs();
-                return bugs;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
+            return _db;
         }
 
         [HttpGet]
         [Route("{id}")]
-        public object getBug(string id)
+        public void getBug(string id)
         {
-            try
-            {
-                var bug = _db.GetBugById(id);
-
-                return bug;
-            }
-            catch (Exception)
-            {
-                return "error";
-            }
+            
         }
 
         [HttpGet]
         [Route("insert")]
         public void insert(string id)
         {
-            try
-            {
-                Bug bug = new Bug
-                {
-                    Title = "inserted bug",
-                    Description = "This bug is inserted with a query",
-                    Status = "not fixed"
-                };
-
-                _db.insertBug(bug);
-            }
-            catch (Exception)
-            {
-                // return "error";
-            }
+            
         }
 
         [HttpGet]
         [Route("update/{id}")]
         public void update(string id)
         {
-            try
-            {
-                Bug newBug = new Bug
-                {
-                    Title = "updated bug",
-                    Description = "This bug is updated with a query",
-                    Status = "fixed"
-                };
-
-                _db.updateBug(id, newBug);
-            }
-            catch (Exception)
-            {
-                // return "error";
-            }
+            
         }
 
         [HttpGet]
         [Route("delete/{id}")]
         public void delete(string id)
         {
-            try
-            {
-                _db.deleteBug(id);
-            }
-            catch (Exception)
-            {
-                // return "error";
-            }
+            
         }
     }
 }
---
+++ Lisa.Breakpoint.WebApi/RavenDB.cs
@@ -0,0 +1,36 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+
+namespace Lisa.Breakpoint.WebApi
+{
+    public class RavenDB
+    {
+
+        public RavenDB()
+        {
+            using (IDocumentStore store = new DocumentStore
+            {
+                Url = "http://localhost:8080/",
+                DefaultDatabase = "breakpoint"
+            }.Initialize())
+            {
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    Bug bug = new Bug
+                    {
+                        Title = "een bug",
+                        Description = "fix de bug",
+                        Status = "Fixed"
+                    };
+
+                    session.Store(bug);
+                    int bugId = bug.Id;
+
+                    session.SaveChanges();
+
+                    Bug loadedBug = session.Load<Bug>(bugId);
+                }
+            }
+        }
+    }
+}
