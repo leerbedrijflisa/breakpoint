021e4951ac928effc164f88a4eea877f536c9026
Joost Stam <joost.stam@gmail.com>
2015-09-17 11:17:28 +0200

    Add user model

---
+++ Lisa.Breakpoint.WebApi/models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace Unknown
+{
+    public class Class
+    {
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+    }
+
+}
