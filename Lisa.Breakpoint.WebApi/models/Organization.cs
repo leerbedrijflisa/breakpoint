58646edb4845b9bee9968c5888ceaca61f68a9a6
Ramon <ramon.meijers@hotmail.com>
2015-09-21 09:56:05 +0200

    Fixed Id and made Organization model

---
+++ Lisa.Breakpoint.WebApi/models/Organization.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Lisa.Breakpoint.WebApi.models
+{
+    public class Organization
+    {
+        public string Id { get; set; }
+        public string Slug { get; set; }
+        public IList<Members> Members{ get; set; }
+        public IList<Projects> Projects { get; set; }
+    }
+
+    public class Members
+    {
+        public string Role { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public class Projects
+    {
+        public string Slug { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Lisa.Breakpoint.WebApi/models/Project.cs
+++ Lisa.Breakpoint.WebApi/models/Project.cs
@@ -4,7 +4,7 @@ namespace Lisa.Breakpoint.WebApi.Models
 {
     public class Project
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Slug { get; set; }
         public string Name { get; set; }
         public IList<Member> Members{ get; set; }
