f91a4dbbc5e50c55aaa874369b2f64d2bb059159
litsher <litsher@gmail.com>
2015-10-01 10:04:36 +0200

    Added code that lets you adjust the status. While this does work, it can't be used as we still haven't figured out what we are gonna do for indexing

--- Lisa.Breakpoint.WebClient/wwwroot/dashboard.js
+++ Lisa.Breakpoint.WebClient/wwwroot/dashboard.js
@@ -4,6 +4,7 @@ import {HttpClient} from 'aurelia-http-client';
 
 export class dashboard {
     constructor() {
+        this.isVisible = false;
         this.http = new HttpClient().configure(x => {
             x.withBaseUrl('http://localhost:10791/');      
             x.withHeader('Content-Type', 'application/json')});
@@ -13,12 +14,18 @@ export class dashboard {
         this.loading = true;
         return this.http.get("reports").then( response => {
             this.reports = response.content;
-
-            // this.reports.idNumber = this.reports.Id.split("/")[1];
-
-
             console.log(response.content);
             console.log(response.statusCode); // Might come in handy
+            this.loading = false;
+        });
+    }
+    submit() {
+        var data = {
+            Status: this.status,
+        };
+        console.log(this.status);
+        this.http.post('reports/patch', data).then( response => {
+            window.location.replace("http://localhost:10874/#/dashboard");
             this.loading = false;
         });
     }
