1bdd9a174b721cf55f415d4ff2e3c4740bb0c373
Joost Stam <joost.stam@gmail.com>
2015-10-09 11:24:15 +0200

    added version number on /version

---
+++ Lisa.Breakpoint.WebApi/controllers/VersionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Lisa.Breakpoint.WebApi
+{
+    [Route("")]
+    public class VersionController : Controller
+    {
+        [Route("version")]
+        [HttpGet]
+        public string Get()
+        {
+            string VersionNumber = "v2";
+
+            return VersionNumber;
+        }
+    }
+}
